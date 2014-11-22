4125bf0ce946d9687ca50d6ac7edbcf3a291d59d
tejafabjan <tejafabjan@gmail.com>
2014-11-22 13:11:16 +0100

    code for assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create a special matrix
+## object that stores a numeric vector and cache's its inverse
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that 
+## can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+mat <- matrix(1:2, nrow = 2, ncol = 2) # test 1
+mat <- matrix(c(1,3,9,7,5,3,9,5,4), nrow = 3, ncol = 3)  # test 2
+solve(mat)  # check if it is invertable
+matObj <- makeCacheMatrix(mat)
+# class(matObj) # check if it is a list
+
+cacheSolve(matObj)
+cacheSolve(matObj)
+
